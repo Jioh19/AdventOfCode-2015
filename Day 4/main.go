d5da80c771a390a5c078da20d07d4b8f8f4e5ccc
jioh19 <jioh1982@gmail.com>
2024-01-08 01:11:11 -0300

    done day 4 part 1

---
+++ Day 4/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func main() {
+	data := []byte("yzbqklnj")
+	len := len(data)
+	hash := fmt.Sprintf("%x", md5.Sum(data))
+	s := time.Now()
+	for i := 0; hash[:5] != "00000"; i++ {
+		num := []byte(strconv.Itoa(i))
+		data = append(data[:len], num...)
+		hash = fmt.Sprintf("%x", md5.Sum(data))
+	}
+
+	fmt.Println("Part 1:", string(data))
+	fmt.Println("Time in nanoseconds:", time.Since(s).Nanoseconds())
+
+}
